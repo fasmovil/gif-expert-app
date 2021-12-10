5fbc063bceb2b1e79669be600b66dcf23ed8a6bc
Fernando SNZ <fas.movil@gmail.com>
2021-12-10 19:58:37 -0300

    https glyph used

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category ) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=rVS2uRE3cOqXBzs4oTT4029ULqRwQE8K&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=rVS2uRE3cOqXBzs4oTT4029ULqRwQE8K&limit=10`;
   const response = await fetch(url);
   const {data} = await response.json();
   const gifs = data.map(img => {
